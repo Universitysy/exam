46fb7d2c09eac6b39bdfabc5deecbe329f9c23c9
sidra <sidra.mamghmumi@gmail.com>
2023-03-28 13:39:45 +0300

    front end commit

--- University_Ex/university/exam/views.py
+++ University_Ex/university/exam/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Teacher
 
-# Create your views here.
+def home(request):
+    teachers = Teacher.objects.all()
+    context = {
+        'teachers': teachers
+
+        }
+    return render(request, 'pages/index.html', context)
+
+
+def login(request):
+     return render(request, 'login/login.html', {})
